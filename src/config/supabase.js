a094ec0eef59610882ccc7102b52fb63dab8d8e6
Jerdon Philip G. Macaraeg <jerdonphilipmacaraeg@gmail.com>
2025-10-20 18:49:27 +0800

    main - removed .dotenv in supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,5 @@
 // src/config/supabase.js
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
